d99d699e1faae88758e128f0b4ec015c888a1ab4
kaynote <ektjttnfp5@gmail.com>
2026-06-09 12:48:35 +0900

    Add files via upload

--- people.js
+++ people.js
@@ -2002,3 +2002,77 @@ const people = [
     "tags": []
   }
 ]
+
+const container = document.getElementById("peopleContainer");
+
+function renderPeople(list){
+
+    container.innerHTML = "";
+
+    list.forEach((person, index) => {
+
+        const card = document.createElement("div");
+
+        card.className = "card";
+
+        const imageHTML = person.image
+            ? `<img class="photo" src="${person.image}">`
+            : `<div class="photo"></div>`;
+
+        const tagsHTML = person.tags
+            .map(tag => `<span class="tag">${tag}</span>`)
+            .join("");
+
+        card.innerHTML = `
+
+            ${imageHTML}
+
+            <div class="info">
+
+                <div class="name">
+                    ${index + 1}. ${person.name}
+                </div>
+
+                <div class="meta">
+                    ${person.meta}
+                </div>
+
+                <div class="desc">
+                    ${person.desc}
+                </div>
+
+                <div>
+                    ${tagsHTML}
+                </div>
+
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+renderPeople(people);
+
+document
+.getElementById("searchBox")
+.addEventListener("input", function(){
+
+    const keyword = this.value.toLowerCase();
+
+    const filtered = people.filter(person => {
+
+        return (
+
+            person.name.toLowerCase().includes(keyword) ||
+
+            person.meta.toLowerCase().includes(keyword) ||
+
+            person.desc.toLowerCase().includes(keyword) ||
+
+            person.tags.join(" ").toLowerCase().includes(keyword)
+        );
+    });
+
+    renderPeople(filtered);
+});
