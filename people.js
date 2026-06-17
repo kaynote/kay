c74d5f500de869df59e35a7dba9e996886f539e7
kaynote <ektjttnfp5@gmail.com>
2026-06-18 00:50:46 +0900

    Add files via upload

--- people.js
+++ people.js
@@ -1091,9 +1091,9 @@ const people = [
   },
   {
     "no": 137,
-    "name": "Juawa",
+    "name": "Juana",
     "ko": "와나 or 후아나",
-    "displayName": "Juawa 와나 or 후아나",
+    "displayName": "Juana 와나 or 후아나",
     "note": "산체스배 댄스 콘테스트에 참가 신청했으나 순번을 놓쳐 대회 포기, <span class=\"timestamp\"> 2026.06.05 37:16</span>",
     "image": "no-image.jpg"
   },
